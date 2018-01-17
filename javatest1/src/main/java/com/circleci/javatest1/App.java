1895f833a5eae258b0c7049a55955c0cf194f085
SyedSulaimanM <syedsulaiman.m@ideas2it.com>
2018-01-17 12:29:28 +0530

    Added: java test1 is added.

---
+++ javatest1/src/main/java/com/circleci/javatest1/App.java
@@ -0,0 +1,13 @@
+package com.circleci.javatest1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
